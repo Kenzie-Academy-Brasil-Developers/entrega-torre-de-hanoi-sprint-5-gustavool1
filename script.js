aa37c4744c1989aed75993fc49510772fbc3b9f7
gustavo <go8895806@gmail.com>
2021-08-04 16:22:40 -0300

    validando movimentos

--- script.js
+++ script.js
@@ -1,59 +1,72 @@
 window.onload = criarTorres()
-let discoAtualId=''
+let discoAtualId
 let torreAtual = ''
+let jogada2 = ''
+let arr = []
+
+let cont = 0
+let second = ''
+let retorno ;
+
 function criarTorres(){
     let discColors = ['120, 143, 219','167, 145, 228','186, 230, 136','228, 129, 72']
     let main = document.getElementById("main1");
-    let divEsq = document.createElement("div")
-    divEsq.id = 'divEsq'
-    divEsq.classList.add("torres")
+    for(let i=0; i<=2;i++){
+        let torre = document.createElement("div")
+        torre.classList.add("torres")
+        torre.id = i
+        torre.addEventListener("click", clicado)
+            
+        main.appendChild(torre)
+        
+    }
+    let torreEsq = document.getElementById("0")
     let larguraDisco = 30
     for(let i=0; i<4;i++){
         let disco = document.createElement("div")
-        disco.addEventListener("click",function(event){
-            discoAtualId =event.target.id
-            console.log(discoAtualId)
-        })
         disco.classList.add("discos")
         disco.id=`disco${i}`
         disco.style.width=`${larguraDisco}px`
         disco.style.height=`20px`
         disco.style.background=`rgb(${discColors[i]})`
-        divEsq.appendChild(disco)
+        torreEsq.appendChild(disco)
         larguraDisco +=20 
     }
-    divEsq.addEventListener("click", function(event){
-        torreAtual = event.target.id
-        moverDisco(discoAtualId,torreAtual)
-    })
-    main.appendChild(divEsq)
-    let divMeio = document.createElement("div")
-    divMeio.id = 'divMeio'
-    divMeio.classList.add("torres")
-    divMeio.addEventListener("click",function(event){
-        torreAtual = event.target.id
-        moverDisco(discoAtualId,torreAtual)
-    })
-    main.appendChild(divMeio)
-    let divDir = document.createElement("div")
-    divDir.id = 'divDir'
-    divDir.classList.add("torres")
-    divDir.addEventListener("click",function(event){
-        torreAtual = event.target.id
-        moverDisco(discoAtualId,torreAtual)
-    })
-    main.appendChild(divDir)
-    
 }
 
 
+function clicado(event){
+    let first = event.target
+    
+       
+    arr.push(first)  
+    
+   
+   if(arr.length === 2){
+        let segundoClique = document.getElementById(arr[1].id)
+      
+        if(segundoClique.firstChild !==null){
+            let primeiroDisco = segundoClique.firstChild
+            if(primeiroDisco.style.width >arr[0].firstChild.style.width){
+                
+                segundoClique.insertBefore(arr[0].firstChild, arr[1].firstChild)
+                
+                validacao(segundoClique)
+            }
+        }if(segundoClique.firstChild === null){
+            
+            segundoClique.appendChild(arr[0].firstChild)
+            
+        }
+       
+        arr = new Array()
+   }
+   
+}
 
-function moverDisco(discoAtualId,torreAtual){
-    let disco = document.getElementById(discoAtualId)
-    let torre = document.getElementById(torreAtual)
-    torre.append(disco)
-    disco =''
-    torre=''
+
+function validacao(torre){
+    
 }
 
 function validar(){
