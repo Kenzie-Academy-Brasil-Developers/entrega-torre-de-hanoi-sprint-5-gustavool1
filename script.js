0959305c6ad959f01adf776ca2e87e8b079b06d2
gustavo <go8895806@gmail.com>
2021-08-04 19:50:11 -0300

    css Basicão, perdão richard s2

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-window.onload = criarTorres()
-let discoAtualId
-let torreAtual = ''
-let jogada2 = ''
+let btnJogar = document.getElementById("btnJogar")
+btnJogar.addEventListener('click',criarTorres)
 let arr = []
-
-let cont = 0
-let second = ''
-let retorno ;
-
+let main = document.getElementById("main1");
 function criarTorres(){
+   
+    btnJogar.style.display='none'
     let discColors = ['120, 143, 219','167, 145, 228','186, 230, 136','228, 129, 72']
     let main = document.getElementById("main1");
     for(let i=0; i<=2;i++){
@@ -38,36 +34,40 @@ function criarTorres(){
 function clicado(event){
     let first = event.target
     arr.push(first)  
+    let move = document.createElement('p')
     if(arr.length === 2){
         let segundoClique = document.getElementById(arr[1].id)
-      
         if(segundoClique.firstChild !==null){
             let primeiroDisco = segundoClique.firstChild
             if(primeiroDisco.style.width >arr[0].firstChild.style.width){
-                
                 segundoClique.insertBefore(arr[0].firstChild, arr[1].firstChild)
-                
-                validacao(segundoClique)
-            }
-        }if(segundoClique.firstChild === null){
-            
-            segundoClique.appendChild(arr[0].firstChild)
-            
+                if(segundoClique.id !=='0'){
+                    validar(segundoClique)
+                }
+            }else{
+                let section = document.getElementById("sectionInfo")
+                move.classList.add('derrota')
+                move.innerHTML='Movimento inválido'
+                setTimeout(()=>{
+                    move.remove()
+                },2000)
+                section.appendChild(move)
         }
-       
+        }if(segundoClique.firstChild === null){
+            segundoClique.appendChild(arr[0].firstChild)
+        }
         arr = new Array()
    }
    
 }
-
-
-function validacao(torre){
-    
-}
-
-function validar(){
+function validar(torre){
     let filhosQuant =torre.childElementCount;
     if(filhosQuant === 4){
-            alert("WINNNN");
+        let section = document.getElementById("sectionInfo")
+        let move = document.createElement('p')
+        move.classList.add('vitoria')
+        move.innerHTML='Vitoria'
+        section.appendChild(move)
+        
     }
 }
