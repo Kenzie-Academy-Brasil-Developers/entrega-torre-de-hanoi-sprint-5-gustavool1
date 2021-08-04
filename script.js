eac0396e2ac91ee6ef97a9fdedd428b1947d84de
gustavool1 <64279418+gustavool1@users.noreply.github.com>
2021-08-04 07:17:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,58 @@
 window.onload = criarTorres()
+let discoAtualId
+let torreAtual = ''
+let jogada2 = ''
+let torre = []
+let cont = 0
 function criarTorres(){
     let discColors = ['120, 143, 219','167, 145, 228','186, 230, 136','228, 129, 72']
     let main = document.getElementById("main1");
-    let divEsq = document.createElement("div")
-    divEsq.id = 'divEsq'
-    divEsq.classList.add("torres")
+    for(let i=0; i<=2;i++){
+        let torre = document.createElement("div")
+        torre.classList.add("torres")
+        torre.id = i
+        torre.addEventListener("click",function(event){
+            let firstClick = event.currentTarget.id
+            let jog2 = ''
+            let checando = ''
+            let torres = document.getElementsByClassName('torres')
+            for(let i=0; i<torres.length;i++){
+                
+                torres[i].addEventListener("click", function(el){
+                    jogada2 =el.currentTarget.id
+                    cont ++ 
+                })
+                
+            }
+            console.log(cont)
+           if(cont>0){
+            moverDisco(firstClick, jogada2)
+           }
+            
+        })
+        main.appendChild(torre)
+    }
+    let torreEsq = document.getElementById("0")
     let larguraDisco = 30
     for(let i=0; i<4;i++){
         let disco = document.createElement("div")
+        
         disco.classList.add("discos")
         disco.id=`disco${i}`
         disco.style.width=`${larguraDisco}px`
         disco.style.height=`20px`
         disco.style.background=`rgb(${discColors[i]})`
-        divEsq.appendChild(disco)
+        torreEsq.appendChild(disco)
         larguraDisco +=20 
     }
-    main.appendChild(divEsq)
-    let divMeio = document.createElement("div")
-    divMeio.id = 'divMeio'
-    divMeio.classList.add("torres")
-    main.appendChild(divMeio)
-    let divDir = document.createElement("div")
-    divDir.id = 'divDir'
-    divDir.classList.add("torres")
-    main.appendChild(divDir)
-    
+   
 }
 
 
 
-function moverDisco(){
-
+function moverDisco(primeiroClick,segundoClick){
+    console.log(primeiroClick)
+    console.log(segundoClick)
 }
 
 function validar(){
